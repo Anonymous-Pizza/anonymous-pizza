062cb46eeabc669dad95a6f83b01244785091b85
lichin shao <lichinshao@gmail.com>
2017-11-30 19:32:25 -0800

    added bullet points to instructions for workout

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,12 +1,20 @@
-var Exercise = (props) => (
-  <div className="exercise">
+var Exercise = (props) => {
+  var description = props.exercise.description;
+  var instructions = description.split('.');
+  console.log('instructons', instructions);
+  return(
+    <div className="exercise">
     <div className="exerciseDescription">
       <img className="exerciseImage" src={props.exercise.picture} />
       <p><span className="exerciseName">{props.exercise.name}</span></p>
-      {props.exercise.description}
+      {instructions.map(line => (
+          <li>{line}</li>
+        ))}
     </div>
   </div>
-);
+  )
+
+};
 
 
 window.Exercise = Exercise;
